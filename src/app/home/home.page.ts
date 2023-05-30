54d0844b2976bf705b8256edda2276dc537aabee
Vijay Adithya B K <vijayadithyabk@gmail.com>
2023-05-30 10:05:19 +0530

    Added Insure project live link.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -32,7 +32,7 @@ export class HomePage {
     'In-Sure': {
       description:
         'A material-designed website to choose an insurance which they need from the various insurance policies available. They can even claim their insurance from the website by providing the basic details and uploading the required documents.',
-      website: 'https://example.com/in-sure',
+      website: 'https://vijayadithyabk.github.io/Insure/',
     },
     'Recognition of Human Actions': {
       description:
